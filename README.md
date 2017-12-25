e95646fd33954bb8b767e97417ef22a6a73c9b9c
Pumych <pumych@gmail.com>
2017-12-25 15:29:08 +0200

    Main functionality

---
+++ README.md
@@ -0,0 +1,16 @@
+Chrome extension that extends Bittrex market functionality.
+
+# Features
+1. Showing USD price at closed date in "MY ORDER HISTORY" section.
+
+# Installation
+* Goto Chrome Settings -> More Tools -> Extensions
+* Check "Developer mode" checkbox
+* Click "Load unpacked extension" and select extension folder.
+
+# TBD
+
+# Known bugs
+
+# Warranty
+Please use this extension without any warranty at your own risk.
---
+++ scripts.js
@@ -0,0 +1,48 @@
+console.log(' :: Bittrex extender: scripts.js Loaded');
+
+let checkedTimes = 0;
+let coin = '';
+
+$(document).ready(function(){
+
+    $(function(){
+        console.log(' :: Bittrex extender: document ready');
+        let url = window.location.toString();
+        coin = url.split('-')[1];
+        checkContainer();
+    });
+});
+
+function elementIsVisible(){
+    console.log(' :: Bittrex extender: element appeared!');
+    let times = $('#closedMarketOrdersTable td.date.sorting_1');
+
+    for(let i=0; i< times.length; i++){
+        let time = new Date(times[i].innerText).getTime()/1000;
+        $.get(`https://min-api.cryptocompare.com/data/pricehistorical?fsym=${coin}&tsyms=USD&ts=${time}`, function(data){
+            times[i].innerHTML = times[i].innerHTML + `<span style="font-weight: bold; color: red" title="Price in USD at closed date"> ( $${data[coin]['USD']} )</span>`;
+        }).always(function(msg) {
+            console.log(msg);
+        });
+    }
+}
+
+function checkContainer () {
+    checkedTimes++;
+    if($('#closedMarketOrdersTable td.date.sorting_1').is(':visible')){ //if the container is visible on the page
+        elementIsVisible();
+    } else {
+        setTimeout(checkContainer, 500); //wait 50 ms, then try again
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
