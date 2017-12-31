10ce76f4cb8098d5584cef43eeb9ee49f5becde9
Pumych <pumych@gmail.com>
2017-12-31 10:29:44 +0200

    Styles moved, readme updated

--- scripts.js
+++ scripts.js
@@ -19,12 +19,35 @@ function elementIsVisible(){
 
     for(let i=0; i< times.length; i++){
         let time = new Date(times[i].innerText).getTime()/1000;
+        let before = 0;
+
         $.get(`https://min-api.cryptocompare.com/data/pricehistorical?fsym=${coin}&tsyms=USD&ts=${time}`, function(data){
-            times[i].innerHTML = times[i].innerHTML + `<span class="bte-text" title="Price in USD at closed date"> ( $${data[coin]['USD']} )</span>`;
+            before = data[coin]['USD'];
+            times[i].innerHTML = times[i].innerHTML + `<span class="bte-text" title="Price in USD at closed date"> | $${before} </span>`;
+            getCurrent(coin, times, before, i);
         }).always(function(msg) {
             console.log(msg);
         });
     }
+}
+
+function getCurrent(coin, times, before, i){
+    let now = 0;
+    let currTime = Math.floor(new Date().getTime()/1000);
+    $.get(`https://min-api.cryptocompare.com/data/pricehistorical?fsym=${coin}&tsyms=USD&ts=${currTime}`, function(data){
+        now = data[coin]['USD'];
+        let delta = calcDelta(before, now);
+        let profitClass = delta >= 0 ? 'green' : 'red';
+        times[i].innerHTML = times[i].innerHTML + `<span class="bte-text ` + profitClass + `" title="Delta price"> ( ${delta}% )</span>`;
+    }).always(function(msg) {
+        console.log(msg);
+    });
+}
+
+function calcDelta(before, now){
+    if( now == before ) return 0;
+    let delta = now > before ? ((now-before)/before)*100 : -((before-now)/before)*100;
+    return delta.toFixed(1);
 }
 
 function checkContainer () {
